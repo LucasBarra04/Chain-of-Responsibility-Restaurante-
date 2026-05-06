4c7c29a446136322ddd252cb84612dcfac406482
Lucas Barra Machado <lucasbm.jf@gmail.com>
2026-05-05 21:56:49 -0300

    Pedido

---
+++ Restaurante/src/Classes/Pedido.java
@@ -0,0 +1,8 @@
+package Classes;
+
+public class Pedido {
+    private TipoPedido tipoPedido;
+
+    public TipoPedido getTipoPedido()              { return tipoPedido; }
+    public void setTipoPedido(TipoPedido tipoPedido) { this.tipoPedido = tipoPedido; }
+}
